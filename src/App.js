f7a3a5e6c003b60048e2f49898b88786e140e28a
josephenoch <enochjoseph01@gmail.com>
2022-03-30 21:56:18 +0100

    cleaned up cra template and installed firebase and mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Typography,  } from "@mui/material";
+import { makeStyles } from '@mui/styles';
 
-function App() {
+const useStyle =  makeStyles({
+  app:{
+    display:"flex",
+    width:"100vw",
+    height:"100vh",
+    backgroundColor:"blue"
+  },
+  sideBar:{
+    flex:"0.3",
+    backgroundColor:"pink"
+  }
+}) 
+export const App = () => {
+  const classes = useStyle()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className={classes.app}>
+      <Box className={classes.sideBar}>
+
+      </Box>
+    </Box>
   );
 }
 
-export default App;
+ 
