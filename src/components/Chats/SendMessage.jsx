0e4aab486fc584161ffb909801d7d197ee65faff
josephenoch <enochjoseph01@gmail.com>
2022-04-03 21:42:07 +0100

    started working with emoji-picker-react

--- src/components/Chats/SendMessage.jsx
+++ src/components/Chats/SendMessage.jsx
@@ -1,8 +1,10 @@
-import React,{ useState} from 'react'
+import React,{useState} from 'react'
 
-import {Box, Button} from "@mui/material"
+
+import {Box, IconButton} from "@mui/material"
 import { EmojiEmotionsOutlined } from '@mui/icons-material'
 import { makeStyles } from '@mui/styles'
+import { Emoji } from '../Emoji'
 
 const useStyles = makeStyles({
     root:{
@@ -33,34 +35,47 @@ const useStyles = makeStyles({
 
 export const SendMessage = () => {
   const [message, setMessage] = useState("")
+  const [modal, setModal] = useState(false)
   const classes = useStyles() 
+  const handleModal = () => {
+    setModal(!modal)
+  }
   const sendMessage = (e) =>{
       e.preventDefault()
       setMessage("")
   }
+
   return (
     <Box
         className={classes.root}
     >
-        <EmojiEmotionsOutlined
-            sx={{
-                color:"gray"
-            }}
-        />
-        <Box className={classes.inputBox}>
-            <form>
-                <input
-                    value={message}
-                    className={classes.sendInput}
-                    onChange={(e)=>setMessage(e.target.value)}
-                />
-                <button
-                    type="submit"
-                    onClick={sendMessage}
-                    style={{display:"none"}}
-                ></button>
-            </form>
-        </Box>
+        <IconButton
+            onClick={handleModal}
+        >
+            <EmojiEmotionsOutlined
+                
+                sx={{
+                    color:"gray"
+                }}
+            />
+        </IconButton>
+        <Emoji
+            modal={modal}
+            handleModal ={handleModal}
+            setMessage={setMessage}
+        />  
+        <form className={classes.inputBox}>
+            <input
+                value={message}
+                className={classes.sendInput}
+                onChange={(e)=>setMessage(e.target.value)}
+            />
+            <button
+                type="submit"
+                onClick={sendMessage}
+                style={{display:"none"}}
+            ></button>
+        </form>
     </Box>
   )
 }
---
+++ src/components/Emoji.jsx
@@ -0,0 +1,26 @@
+import { Modal } from '@mui/material'
+import React from 'react'
+import Picker from "emoji-picker-react"
+
+export const Emoji = ({modal,handleModal,setMessage}) => {
+  return (
+    <Modal
+        open={modal}
+        onClose={handleModal}
+        sx={{
+            position: 'fixed',
+            top: "59%",
+            left: '35%',
+            transform: 'translate(-50%, -50%)',
+            width: "10vw",
+            height:"20vh",
+        }}
+    >
+        <Picker 
+            onEmojiClick={
+                (e,emojiObject)=>setMessage(
+                    prevState=>prevState
+                    ?prevState+emojiObject.emoji:emojiObject.emoji)} />
+    </Modal>
+  )
+}
