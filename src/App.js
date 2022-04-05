2bf6086aa544e8727bfcc556ba5adf3e9018dc84
josephenoch <enochjoseph01@gmail.com>
2022-04-05 09:54:37 +0100

    finished understanding crud with firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Box } from "@mui/material";
-import "./App.css"
+// import "./App.css"
 
-import { Sidebar } from "./components/Sidebar/Sidebar";
-import MessagBackground from "./assets/chatbg.png"
-import { Chats } from "./components/Chats/Chats";
-import {mainUser} from "./fakeData" 
+// import { Sidebar } from "./components/Sidebar/Sidebar";
+// import MessagBackground from "./assets/chatbg.png"
+// import { Chats } from "./components/Chats/Chats";
+// import {mainUser} from "./fakeData" 
 import { db } from "./firebase-config";
-import {collection, getDocs} from "firebase/firestore"
+import {collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from "firebase/firestore"
 import { useEffect, useState } from "react";
 
 
 
 export const App = () => {
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
   const [users, setUsers] = useState([])
   const usersCollectionRef = collection(db, "users")
   useEffect(()=>{
@@ -21,7 +23,28 @@ export const App = () => {
       setUsers(data.docs.map((doc)=> ({...doc.data(), id:doc.id})))
     }
     getUsers()
-  }, [])
+  }, [usersCollectionRef])
+
+  const addNewUser = async () =>{
+    if(newName !== "" && newAge!==0){
+      await addDoc(usersCollectionRef, {name:newName, age: Number(newAge)})
+    }
+    else{
+      console.error("Add Adequate Details")
+    }
+  }
+
+  const editAge = async (id, age) => {
+    const userDoc = doc(db, "users", id)
+    const newField = {age:age+1}
+    await updateDoc(userDoc,newField)
+
+  }
+
+  const delterDoc = async(id) =>{
+    const userDoc = doc(db, "users", id)
+    await deleteDoc(userDoc)
+  }
   
   return (
     <Box className="app">
@@ -35,12 +58,29 @@ export const App = () => {
           user={mainUser}
         />
       </Box> */}
+      <input 
+        type="text" 
+        value={newName}
+        onChange={e => setNewName(e.target.value)}
+      />
+      <input 
+        type="number"
+        value={newAge}
+        onChange={e => setNewAge(e.target.value)}
+      />
+      <button 
+        onClick={addNewUser}
+      >
+          Add User
+        </button>
       <>{users.map(user =>{
         return (
           <div key={user.id  }>
             {" "}
             <h1>{user.name}</h1>
             <h2>{user.age}</h2>
+            <button onClick={() => editAge(user.id,user.age)} >Increment Age</button>
+            <button onClick={() => delterDoc(user.id)} >Delete Doc</button>
           </div>
         )
 
